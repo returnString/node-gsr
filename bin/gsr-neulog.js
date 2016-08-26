fd8824bd2f830f9615218f4fc7c22f6e791df5aa
Ruan Pearce-Authers <ruanpa@outlook.com>
2016-08-26 01:31:06 +0100

    Allow custom intervals in gsr-neulog

--- bin/gsr-neulog.js
+++ bin/gsr-neulog.js
@@ -2,14 +2,14 @@
 'use strict';
 
 const { NeuLogGsr } = require('../');
-const [ port ] = process.argv.slice(2);
+const [ port, interval = 100 ] = process.argv.slice(2);
 
 if (!port)
 {
 	return console.error('Please supply the path to a serial port');
 }
 
-const logger = new NeuLogGsr(port);
+const logger = new NeuLogGsr(port, { interval });
 logger.on('data', (value, timestamp) =>
 {
 	console.log(value, timestamp);
