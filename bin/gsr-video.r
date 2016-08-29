25a5b9944f3d15ff694886cf5d669c44ace73dd1
Ruan Pearce-Authers <ruanpa@outlook.com>
2016-08-29 02:36:46 +0100

    Better readability for gsr-video output

--- bin/gsr-video.r
+++ bin/gsr-video.r
@@ -13,6 +13,7 @@ parser$add_argument('output', help = 'output video file')
 parser$add_argument('--fps', type = 'integer', default = 3, help = 'frames per second')
 parser$add_argument('--dpi', type = 'integer', default = 72, help = 'dpi for generated video frames')
 parser$add_argument('--tempdir', help = 'working directory for intermediate files')
+parser$add_argument('--graphcolour', default = 'red')
 args <- parser$parse_args()
 
 main <- function()
@@ -23,8 +24,9 @@ main <- function()
 	output <- args$output
 	dpi <- args$dpi
 	frameDir <- if (is.null(args$tempdir)) tempdir() else args$tempdir
+	graphColour <- args$graphcolour
 
-	videoAttrs <- system(paste0('ffprobe -v 1 -show_entries stream=width,height,r_frame_rate ',
+	videoAttrs <- system(paste0('ffprobe -v error -show_entries stream=width,height,r_frame_rate ',
 		'-of default=noprint_wrappers=1:nokey=1 ', gameplayInput), intern = T)
 
 	videoRes = as.integer(videoAttrs[1:2])
@@ -49,26 +51,29 @@ main <- function()
 	foreach(i = 1:frames, .packages = 'ggplot2') %dopar% {
 		toRender <- subset(data, offset < i / fps * 1000)
 		plot <- ggplot(toRender, aes(x = offsetSeconds, y = value)) +
-			geom_line() +
+			geom_line(color = graphColour, size = 2) +
 			labs(x = 'Time (s)', y = 'GSR (microsiemens)') +
 			ylim(c(0, ceiling(max(data$value)))) +
-			xlim(c(0, duration))
-		ggsave(paste0(frameDir, '/', i, '.jpg'), plot,
-			width = videoSize[1], height = videoSize[2], dpi = dpi)
+			xlim(c(0, duration)) +
+			theme(plot.background = element_rect(fill = 'transparent'),
+				panel.background = element_blank())
+		ggsave(paste0(frameDir, '/', i, '.png'), plot,
+			width = videoSize[1], height = videoSize[2], dpi = dpi, bg = 'transparent')
 	}
 
 	stopCluster(cluster)
 
-	fpsRatio <- gameplayFps / fps 
-	tempVideoPath = paste0(frameDir, '/', 'frames.mp4')
+	fpsRatio <- gameplayFps / fps
+	tempVideoPath = paste0(frameDir, '/', 'frames.mov')
 	message('Rendering video from frames: ', tempVideoPath)
-	system(paste0('ffmpeg -v 1 -y -r ', gameplayFps, ' -framerate ', fps,
-		' -i "', frameDir, '/%d.jpg" ', ' -filter:v "setpts=', fpsRatio, '*PTS" ', tempVideoPath))
+	system(paste0('ffmpeg -v error -y -r ', gameplayFps, ' -framerate ', fps,
+		' -i "', frameDir, '/%d.png" ',
+		' -filter:v "setpts=', fpsRatio, '*PTS" -vcodec qtrle ', tempVideoPath))
 
 	message('Merging with gameplay')
-	system(paste0('ffmpeg -v 1 -y -r ', gameplayFps, ' -i "', gameplayInput, '" -i "', tempVideoPath, '" ',
-		' -filter_complex "[1:0]format=rgba[a];',
-		'[0:0]setdar=', dar, ',format=yuva420p,colorchannelmixer=aa=0.8[b];',
+	system(paste0('ffmpeg -v error -y -r ', gameplayFps, ' -i "', gameplayInput, '" -i "', tempVideoPath, '" ',
+		' -filter_complex "[0:v]format=rgba[a];',
+		'[1:v]setdar=', dar, ',format=yuva420p,colorchannelmixer=aa=0.1[b];',
 		'[a][b]overlay=shortest=1"',
 		' ', output))
 }
